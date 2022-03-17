2f345851f17ab774776fbae1115444bac5c4ea83
LeoLacana <wilson17414@gmail.com>
2022-03-17 18:53:39 -0300

    req 3 done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    print(instance.queue_list)
+    
+    if len(instance) != 0 and path_file in instance.queue_list:
+        return None
+    else:
+        instance.enqueue(path_file)
+        return sys.stdout.write(str(result))
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,18 @@
 class Queue:
     def __init__(self):
-        self.queue = []
+        self.queue_list = []
 
     def __len__(self):
-        return len(self.queue)
+        return len(self.queue_list)
 
     def enqueue(self, value):
-        self.queue.append(value)
+        self.queue_list.append(value)
 
     def dequeue(self):
-        return self.queue.pop(0)
+        return self.queue_list.pop(0)
 
     def search(self, index):
         if index >= 0:
-            return self.queue[index]
+            return self.queue_list[index]
         else:
             raise IndexError('Invalid index')
